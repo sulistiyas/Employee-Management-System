1ff0c922ffacaad9633cfdc6d5eb62e801b91f46
sulistiyas <sulisgor.a@gmail.com>
2026-06-30 10:14:12 +0700

    fix : sidebar footer

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -132,10 +132,28 @@
 
     </nav>
 
-    {{-- Collapse toggle --}}
-    <button class="ems-sidebar__toggle" @click="sidebarOpen = !sidebarOpen">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" x-show="sidebarOpen"><polyline points="15 18 9 12 15 6"/></svg>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" x-show="!sidebarOpen"><polyline points="9 18 15 12 9 6"/></svg>
-    </button>
+    {{-- Footer actions --}}
+    <div class="ems-sidebar__footer">
+
+        <a href="https://wa.me/6282110873602" target="_blank" rel="noopener"
+           class="ems-sidebar__footer-btn ems-sidebar__footer-link" title="Contact Us">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"/></svg>
+            {{-- <span class="ems-sidebar__footer-label" x-show="sidebarOpen">Contact Us</span> --}}
+        </a>
+
+        <form method="POST" action="{{ route('logout') }}" class="ems-sidebar__footer-form">
+            @csrf
+            <button type="submit" class="ems-sidebar__footer-btn" title="Logout">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" y1="12" x2="9" y2="12"/></svg>
+                {{-- <span class="ems-sidebar__footer-label" x-show="sidebarOpen">Logout</span> --}}
+            </button>
+        </form>
+
+        <button class="ems-sidebar__footer-btn ems-sidebar__footer-btn--toggle" @click="sidebarOpen = !sidebarOpen" title="Collapse">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" x-show="sidebarOpen"><polyline points="15 18 9 12 15 6"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" x-show="!sidebarOpen"><polyline points="9 18 15 12 9 6"/></svg>
+        </button>
+
+    </div>
 
 </aside>
